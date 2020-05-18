e076bdbd64eab2a710bdc24834f8255e564ff241
Anton Pavlov <antonio87@bk.ru>
2020-05-18 15:18:54 +0300

    Сделал все задания без *

--- homework_lesson_2.playground/Contents.swift
+++ homework_lesson_2.playground/Contents.swift
@@ -23,4 +23,24 @@ func DelNa3() {
 DelNa3()
 
 
+var arr: [Int] = []
+for i in 0...99 {
+    arr.append(i)
+}
+print("Возрастающий массив из 100 чисел \(arr)")
 
+//for(index, value) in arr.enumerated() {
+//    if (value % 2) == 0 {
+//        arr[index] += 1
+//    }
+//}
+//arr
+func UdalenieChet () {
+    for (_, value) in arr.enumerated() {
+        if (value % 2) == 0{
+            arr.remove(at: arr.firstIndex(of: value)!)
+        }
+    }
+    print("Массив без четных чисел \(arr)")
+}
+UdalenieChet()
