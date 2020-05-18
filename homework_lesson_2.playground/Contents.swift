3d1ad5ea15b2a45c62553fce924d4b3b47a68dd8
Anton Pavlov <antonio87@bk.ru>
2020-05-18 15:00:43 +0300

    сделал второе задание

--- homework_lesson_2.playground/Contents.swift
+++ homework_lesson_2.playground/Contents.swift
@@ -1,4 +1,26 @@
 import UIKit
 
+var a = 23
+var b = 0
+
+func ChetNechet() {
+    if a % 2 == 0 {
+        print("\(a) четное")
+    } else {
+        print("\(a) нечетное")
+    }
+}
+ChetNechet()
+
+func DelNa3() {
+    b = a % 3
+    if b == 0 {
+        print("\(a) делится на 3 без остатка")
+    } else {
+        print("\(a) делится на 3 с остатком \(b)")
+    }
+}
+DelNa3()
+
 
 
